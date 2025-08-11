6318801952070eb7bef4d91b29a474117f53cb7e
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-08-11 15:55:14 -0300

    fix: movimentação

--- src/main/java/chess/pieces/Rook.java
+++ src/main/java/chess/pieces/Rook.java
@@ -1,17 +1,24 @@
+
 package chess.pieces;
 
 import boardgame.Board;
-import chess.Color;
 import chess.ChessPiece;
+import chess.Color;
 
 public class Rook extends ChessPiece {
 
-    public Rook(Board tabuleiro, Color cor) {
-        super(tabuleiro, cor);
+    public Rook(Board board, Color color) {
+        super(board, color);
     }
 
     @Override
     public String toString() {
-        return "T";
+        return "R";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
